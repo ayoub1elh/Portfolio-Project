d5822ff9b6038f9435ecab1e1a28ef31c3ea3b34
ayoub1elh <ay2oub49@gmail.com>
2024-10-02 15:21:32 +0100

    first commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">
+        Joseph ayoub1elh
+      </h1>
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Frontend Developer
+      </p>
+      <p className="text-sm max-w-2xl mb-3 font-bold">
+        A little bit of myself, I'm from Kenya, you can find me online as{" "}
+        <a
+          href="https://ayoub1elh.com"
+          target="_blank"
+          className="text-cyan-500 hover:underline underline-offset-2 decoration-2"
+          rel="noopener"
+        >
+          @ayoub1elh
+        </a>
+        , my experience is in Web Development and I enjoy working in projects
+        related to Back-end
+      </p>
+      <ul className="flex gap-3 mb-6">
+        <li>
+          <a
+            href="https://github.com/ayoub1elh"
+            aria-label="Find me on GitHub"
+            target="_blank"
+            rel="noopener"
+            className="text-cyan-500 hover:underline underline-offset-2 decoration-2"
+          >
+            <i class="fab fa-github" aria-hidden="true"></i> GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/joseph-ayoub1elh-498a52162/"
+            aria-label="Find me on LinkedIn"
+            target="_blank"
+            rel="noopener"
+            className="text-cyan-500 hover:underline underline-offset-2 decoration-2"
+          >
+            <i class="fab fa-linkedin" aria-hidden="true"></i> LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/Joseph_ayoub1elh"
+            aria-label="Find me on Twitter"
+            target="_blank"
+            rel="noopener"
+            className="text-cyan-500 hover:underline underline-offset-2 decoration-2"
+          >
+            <i class="fab fa-twitter" aria-hidden="true"></i> Twitter
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://gitlab.com/ayoub1elh"
+            aria-label="Find me on GitLab"
+            target="_blank"
+            rel="noopener"
+            className="text-cyan-500 hover:underline underline-offset-2 decoration-2"
+          >
+            <i class="fab fa-gitlab" aria-hidden="true"></i> GitLab
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    title: "ALX Checker",
+    imgUrl: "/assets/floreciendoideas.png",
+    stack: ["Python", "Shell", "Bash"],
+    linkCode: "https://github.com/ayoub1elh/alx-checker",
+    link: "",
+  },
+  {
+    title: "Google Homepage",
+    imgUrl: "/assets/google-homepage.png",
+    stack: ["HTML", "CSS"],
+    linkCode: "https://github.com/ayoub1elh/google-homepage",
+    link: "https://ayoub1elh.github.io/google-homepage/",
+  },
+  {
+    title: "ALX Headphones",
+    imgUrl: "/assets/headphones-landing-page.jpg",
+    stack: ["HTML", "CSS"],
+    linkCode: "https://github.com/ayoub1elh/alx-headphones",
+    link: "https://ayoub1elh.github.io/alx-headphones/",
+  },
+  {
+    title: "ALX Smiling School",
+    imgUrl: "/assets/smile-school.png",
+    stack: ["HTML", "CSS", "Bootstrap"],
+    linkCode: "https://github.com/ayoub1elh/alx-smiling-school",
+    link: "https://ayoub1elh.github.io/alx-smiling-school/homepage.html",
+  },
+  {
+    title: "Minimal Portfolio",
+    imgUrl: "/assets/react-minimal-portfolio.png",
+    stack: ["React", "Vite", "Tailwind"],
+    linkCode: "https://github.com/ayoub1elh/minimal-portfolio",
+    link: "https://ayoub1elh.github.io/minimal-portfolio/",
+  },
+  {
+    title: "Computational Bits",
+    imgUrl: "/assets/Landing_page_2020.png",
+    stack: ["React", "NodeJS"],
+    linkCode: "https://github.com/ayoub1elh/computational-bits",
+    link: "https://ayoub1elh.github.io/computational-bits/",
+  },
+  {
+    title: "Simple Shell",
+    imgUrl: "/assets/simple_shell2.png",
+    stack: ["C"],
+    linkCode: "https://github.com/ayoub1elh/simple_shell",
+    link: "",
+  },
+];
